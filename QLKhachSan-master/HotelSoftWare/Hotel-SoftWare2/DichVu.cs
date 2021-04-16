ec81b37d660de39cfa4a5d28e993dae8fa85059b
thuymieu <77791500+thuymieu@users.noreply.github.com>
2021-04-16 09:57:15 +0700

    Update DichVu.cs

--- QLKhachSan-master/HotelSoftWare/Hotel-SoftWare2/DichVu.cs
+++ QLKhachSan-master/HotelSoftWare/Hotel-SoftWare2/DichVu.cs
@@ -1,1 +1,24 @@
 
+namespace Hotel_SoftWare2
+{
+    using System;
+    using System.Collections.Generic;
+    
+    public partial class DichVu
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public DichVu()
+        {
+            this.SuDungDichVus = new HashSet<SuDungDichVu>();
+        }
+    
+        public string MaDV { get; set; }
+        public string TenDV { get; set; }
+        public Nullable<double> DonGia { get; set; }
+        public string MaLoaiDV { get; set; }
+    
+        public virtual LoaiDichVu LoaiDichVu { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<SuDungDichVu> SuDungDichVus { get; set; }
+    }
+}
